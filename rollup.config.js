9ae2828cf1f1ac59dde8c51fe08dd03aab724dc9
James R. Lambert <ryolambert@gmail.com>
2019-06-16 13:50:42 -0600

    Update boiler from original repo & minor fixes

---
+++ rollup.config.js
@@ -0,0 +1,89 @@
+// @flow
+
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+import { minify } from 'uglify-es';
+
+const name = 'Images';
+const path = 'dist/react-images';
+const globals = {
+  classnames: 'classNames',
+  glam: 'glam',
+  'prop-types': 'PropTypes',
+  'react-dom': 'ReactDOM',
+  'react-input-autosize': 'AutosizeInput',
+  'raf-schd': 'rafScheduler',
+  'react-view-pager': 'PageView',
+  'react-full-screen': 'Fullscreen',
+  'react-scrolllock': 'ScrollLock',
+  'a11y-focus-store': 'focusStore',
+  'react-transition-group': 'Transition',
+  react: 'React',
+};
+import createEnv from 'dotenv';
+
+createEnv.config();
+const external = Object.keys(globals);
+const babelOptions = prod => {
+  let result = {
+    babelrc: false,
+    presets: [['env', { modules: false }], 'react'],
+    plugins: [
+      'transform-class-properties',
+      'transform-object-rest-spread',
+      'external-helpers',
+    ],
+  };
+  if (prod) {
+    result.plugins.push('transform-react-remove-prop-types');
+  }
+  return result;
+};
+const injectSecret = () => {
+  return replace({
+    'process.env.UNSPLASH_API_KEY': JSON.stringify(
+      process.env.UNSPLASH_API_KEY
+    ),
+  });
+};
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: path + '.es.js',
+      format: 'es',
+    },
+    external: external,
+    plugins: [babel(babelOptions(false)), injectSecret()],
+  },
+  {
+    input: 'src/index.umd.js',
+    output: {
+      name: name,
+      file: path + '.js',
+      format: 'umd',
+      globals: globals,
+    },
+    external: external,
+    plugins: [babel(babelOptions(false)), injectSecret(), resolve()],
+  },
+  {
+    input: 'src/index.umd.js',
+    output: {
+      name: name,
+      file: path + '.min.js',
+      format: 'umd',
+      globals: globals,
+    },
+    external: external,
+    plugins: [
+      babel(babelOptions(true)),
+      injectSecret(),
+      resolve(),
+      uglify({}, minify),
+    ],
+  },
+];
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+// @flow
+
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+require('dotenv').config();
+
+module.exports = {
+  context: path.resolve(__dirname, 'docs'),
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs/dist'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  devServer: {
+    port: 8000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'react-images': path.resolve(__dirname, 'src/index'),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // $FlowFixMe: This definitely exists here.
+      'process.env.UNSPLASH_API_KEY': `'${process.env.UNSPLASH_API_KEY}'`,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: false,
+      template: path.resolve(__dirname, 'docs/index.html'),
+    }),
+    new CopyWebpackPlugin(['_redirects', 'favicon.ico', 'index.css']),
+  ],
+};
